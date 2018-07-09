38f1bc3b9cef908b8a31cc13eadde67b7834d8a3
José Juan Carlos Mendoza Taveras <juanzero000@gmail.com>
2018-07-09 14:11:22 -0400

    Resolviendo FileNotFoundException

--- src/main/java/_do/com/humano/controller/MainController.java
+++ src/main/java/_do/com/humano/controller/MainController.java
@@ -87,7 +87,7 @@ public class MainController {
             params.put("pEmail", asegurado.getEmail());
             params.put("pFirma", firma);
 
-            JasperReport jasperReport = (JasperReport) JRLoader.loadObject(new ClassPathResource("/static/report/CanvasDemoReport.jasper").getFile());
+            JasperReport jasperReport = (JasperReport) JRLoader.loadObject(new ClassPathResource("/static/report/CanvasDemoReport.jasper").getInputStream());
             JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, new JREmptyDataSource(1));
             byte[] pdfReport = JasperExportManager.exportReportToPdf(jasperPrint);
 
